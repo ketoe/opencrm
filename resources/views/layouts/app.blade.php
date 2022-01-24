5a3df51f34b2a892b29f4b6158d3e6900df1ddeb
WebKey <60527164+ketoe@users.noreply.github.com>
2022-01-24 17:07:21 +0100

    all updates - szkielet aplikacji

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Open Crm</title>
+    <link rel="stylesheet" href="css/app.css">
+    <link href='http://fonts.googleapis.com/css?family=Alata&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <header>
+        <section>
+            <div class="logo"></div>
+        </section>
+    </header>
+    <nav>
+        <ul>
+            <li><div class="icon home"></div></li>
+            <li><div class="icon clients"></div></li>
+            <li><div class="icon orders"></div></li>
+            <li><div class="icon calender"></div></li>
+            <li><div class="icon notes"></div></li>
+        </ul>
+    </nav>
+    <main>
+        <div class="icon home icon-medium"></div>
+    </main>
+    <footer></footer>
+</body>
+</html>
